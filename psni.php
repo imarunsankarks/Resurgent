9e1a203a8eeed3199402bfdc02df53797b2da801
Arun Sankar <arun.resurgent@gmail.com>
2024-03-18 17:52:13 +0530

    added psni page

---
+++ psni.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+
+<head>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> Resurgent AV</title>
+
+
+    <link rel="icon" type="image/png" href="./assets/common/resurgent-logo.webp">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
+
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/header.css" rel="stylesheet">
+    <link href="css/common.css" rel="stylesheet">
+    <link href="css/product.css" rel="stylesheet">
+    <link href="css/psni.css" rel="stylesheet">
+
+    <link href="css/responsive.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <!-- header will be here  -->
+    <?php include('header.php') ?>
+
+    <div class="viewport">
+        <main>
+
+            <div id="banner-trigger">
+                <section id="psni-banner">
+                    <div class="container">
+                        <div class="row g-5 align-items-center justify-content-center">
+                            <div class="col-lg-7">
+                                <div class="psni-banner-content">
+                                    <h1 class="psni-title">Resurgent is a <br>proud member of PSNI</h1>
+                                    <div class="psni-banner-innercontent">
+                                        <p class="">Becoming a member of The Alliance is a sought-after affiliation,
+                                            thanks to the
+                                            high level of ongoing education, industry insight, benchmarking, and
+                                            certifications that PSNI adds to each expert's capabilities. </p>
+                                        <a href="contact.php" aria-label="Know about the company">
+                                            <div class="banner-button1 button-hover1 cta-btn">
+                                                <div class="circle-large"></div>
+                                                <button>About PSNI</button>
+                                                <div class="btn-bg-black"></div>
+                                            </div>
+                                        </a>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-5">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="circle-left">
+                        <img src="./assets/psni/banner-circle-1.svg" alt="">
+                    </div>
+                    <div class="circle-right">
+                        <img src="./assets/psni/banner-circle-2.svg" alt="">
+                    </div>
+                </section>
+            </div>
+
+            <section class="sec-padding bg-white z-9">
+                <div class="d-sm-block z-9">
+                    <div class="continer p-0">
+                        <div class="row justify-content-center">
+                            <div class="psni-scroll-video-head w-auto">
+                                <h2 class="section-title w-auto">Built for the way your world works</h2>
+                                <p class=" w-auto">COLLABORATING. INSPIRING. TRANSFORMING.</p>
+                            </div>
+                        </div>
+                        <div class="product-banner-img position-relative pt-5">
+                            <div class="img-scroll-ht">
+                                <div class="initial-ht"></div>
+                                <div id="button-trigger">
+                                </div>
+                            </div>
+                            <div class="product-img-container">
+                                <div class="product-imgs">
+                                    <div class="product-img">
+                                        <div class="video-psni">
+                                            <video autoplay muted loop class="z-9">
+                                                <source src="./assets/psni/Corporate-1.m4v" type="video/mp4">
+                                            </video>
+                                            <div class="video-circle"></div>
+                                        </div>
+                                        <!-- <img src="./assets/products/surge+/surge-contact.webp" alt="product image"
+                                            class="img-fluid"> -->
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- ---------------------------------------------------why-us---------------------------------- -->
+            <section class="sec-padding pt-0">
+                <div class="container">
+                    <div class="psni-benefits">
+                        <h2 class="section-title text-md-center wow animate__animated animate__fadeInUp">PSNI Alliance makes AV solutions easier
+                        </h2>
+                        <div class="row mt-md-5 justify-content-center">
+                            <div class="col-md-4 wow animate__animated animate__fadeInUp">
+                                <div class="why-card">
+                                    <h3>trusted</h3>
+                                    <div class="d-flex align-items-center mb-2">
+                                        <span class="why-card-line"></span>
+                                        <h3 class="why-second-head mb-0">partner</h3>
+                                    </div>
+                                    <p>As a PSNI Member, Resurgent is acknowledged as a trusted partner in the industry and sets the standard for best practices</p>
+                                </div>
+                            </div>
+    
+                            <div class="col-md-4 wow animate__animated animate__fadeInUp animate__two">
+                                <div class="why-card">
+                                    <h3>tap the</h3>
+                                    <div class="d-flex align-items-center mb-2">
+                                        <span class="why-card-line"></span>
+                                        <h3 class="why-second-head mb-0">network</h3>
+                                    </div>
+                                    <p>PSNI's network provides direct access to advanced technology, training, and support to all its members</p>
+                                </div>
+                            </div>
+    
+                            <div class="col-md-4 wow animate__animated animate__fadeInUp animate__three">
+                                <div class="why-card">
+                                    <h3>easy</h3>
+                                    <div class="d-flex align-items-center mb-2">
+                                        <span class="why-card-line"></span>
+                                        <h3 class="why-second-head mb-0">deployment</h3>
+                                    </div>
+                                    <p>All PSNI members, regardless of deployment type, are equipped to meet your requirements anywhere, anytime</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- ---------------------------------------footer------------------------------- -->
+            <?php include 'footer.php' ?>
+            <!-- <div class="pb-5"></div> -->
+        </main>
+    </div>
+
+
+
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js" crossorigin="anonymous"
+        referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js" crossorigin="anonymous"
+        referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" crossorigin="anonymous"
+        referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/js/bootstrap.min.js" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" crossorigin="anonymous"></script>
+
+    <script src="js/main.js"></script>
+    <!-- <script src="js/home.js"></script> -->
+    <script src="js/common.js"></script>
+
+
+
+    <script>
+    new WOW().init();
+    </script>
+
+
+
+    <script>
+    // <!-- --------------for banner image and content------------------------------------------- -->
+
+    document.addEventListener("DOMContentLoaded", function() {
+        if (window.innerWidth > 767) {
+            gsap.set(".product-img video", {
+                x: () => {
+                    const imageWidth = window.innerWidth > 750 ? 600 : window.innerWidth * 0.9;
+                    const viewportWidth = window.innerWidth;
+                    const xValue = (viewportWidth - imageWidth) / 2;
+                    console.log(xValue);
+
+                    return xValue;
+                },
+                y: "-=25%",
+                scale: 1,
+                width: window.innerWidth > 750 ? 600 : '90vw',
+                height: window.innerWidth > 750 ? 600 : '50vh',
+                borderRadius: window.innerWidth > 750 ? '50%' : '25px'
+            });
+            const animation = gsap.to(".product-img video", {
+                x: 0,
+                y: "+=10%",
+                scale: 1.2,
+                duration: 1,
+                width: '100%',
+                height: '80vh',
+                borderRadius: '0px',
+                ease: "power1.inOut",
+                stagger: 1,
+            });
+
+            ScrollTrigger.create({
+                trigger: ".product-banner-img",
+                start: "top top",
+                end: "bottom bottom",
+                pin: ".product-img-container",
+                animation: animation,
+                scrub: true,
+                // markers: true,
+                threshold: 1,
+            });
+
+
+        }
+    })
+    </script>
+
+    <script>
+    if (window.innerWidth > 767) {
+        gsap.set(".each-feature-card:not(:first-child)", {
+            x: "0%",
+            opacity: 0,
+        });
+
+        const animation = gsap.to(".each-feature-card:not(:first-child)", {
+            x: "0%",
+            opacity: 1,
+            duration: 0.5,
+            stagger: 1,
+            paused: true,
+        });
+
+        ScrollTrigger.create({
+            trigger: ".features-scroll",
+            start: "top top",
+            end: "bottom bottom",
+            pin: ".right",
+            animation: animation,
+            scrub: true,
+            // markers: true,
+            threshold: 0,
+        });
+    }
+    </script>
+
+
+
+    <script>
+    const bannerTimeline = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#banner-trigger",
+            start: "top top",
+            end: "center center",
+            scrub: true,
+            markers: false,
+            invalidateOnRefresh: true,
+        },
+    });
+
+    bannerTimeline.to(
+        ".banner-product-img img", {
+            y: () => 300,
+            scale: 2,
+        },
+        "<"
+    );
+    </script>
+
+</body>
+
+</html>
